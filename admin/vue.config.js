c11597ea03d123d73d96f124d2867447edeaf7bd
omar.mellouli <omar.mellouli12@gmail.com>
2020-11-20 23:26:31 +0100

    resolves #1 Enhance Build process for admin component

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? process.env.PUBLIC_PATH
+    : '/',
   configureWebpack: {
     devServer: {
       host: "localhost",
+      disableHostCheck: true,
     },
     devtool: "source-map",
   },
