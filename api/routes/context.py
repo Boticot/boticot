a1aa9c9f01038b0660a16ec30804eb32cb22f827
Omar MELLOULI <omar.mellouli12@gmail.com>
2020-12-17 23:48:47 +0100

    resolves #11 Add context management with API (#42)

---
+++ api/routes/context.py
@@ -0,0 +1,32 @@
+from . import routes
+from utils import response_template
+from flask import request, jsonify
+import sys
+import json
+sys.path.append('..')
+from flask import current_app
+from context_service import ContextService
+
+@current_app.route('/context/agents/<agent_name>/users/<user_id>', methods=["PUT"])
+def add_context(agent_name, user_id):
+    if request.get_data():
+        body = json.loads((request.get_data()).decode())
+        ContextService.get_instance().insert_user_context(agent_name, user_id, body.get('context_name'), body.get('context_value'))
+        return response_template(200, "Context successfully added for agent {0} and user_id {1}".format(agent_name, user_id))
+    else:
+        return response_template(400, "A body is mandatory inside the request")
+
+@current_app.route('/context/agents/<agent_name>/users/<user_id>/contexts/<context_name>', methods=["DELETE"])
+def remove_context(agent_name, user_id, context_name):
+    ContextService.get_instance().remove_user_context(agent_name, user_id, context_name)
+    return response_template(200, "Context '{0}' successfully deleted for agent {1} and user_id {2}".format(context_name, agent_name, user_id))
+
+@current_app.route('/context/agents/<agent_name>/users/<user_id>', methods=["DELETE"])
+def remove_all_contexts(agent_name, user_id):
+    ContextService.get_instance().remove_all_user_contexts(agent_name, user_id)
+    return response_template(200, "All Contexts are successfully deleted for agent {0} and user_id {1}".format(agent_name, user_id))
+
+@current_app.route('/context/agents/<agent_name>/users/<user_id>', methods=["GET"])
+def get_all_contexts(agent_name, user_id):
+    contexts = ContextService.get_instance().get_user_contexts(agent_name, user_id)
+    return jsonify(contexts)
