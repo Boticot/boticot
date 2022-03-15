548397012aac596eb72134ecf98cc80366f6d9a1
Choubani Amir <amirensit@gmail.com>
2022-03-15 16:56:07 +0100

    resolves #147 Enhance admin build with docker to avoid use of args (#149)

---
+++ admin/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ROOT_DIR=/usr/share/nginx/html
+
+# Replace env vars in JavaScript files
+echo "Replacing env constants in JS"
+
+for file in $ROOT_DIR/js/*.js* $ROOT_DIR/index.html;
+do
+  echo "Processing $file ...";
+
+  sed -i 's|VUE_APP_NLU_SERVICE_URL_VALUE|'${VUE_APP_NLU_SERVICE_URL}'|g' $file 
+  sed -i 's|VUE_APP_NLU_PATH_VALUE|'${VUE_APP_NLU_PATH}'|g' $file
+  sed -i 's|VUE_APP_NLU_ENTRIES_PAGE_SIZE_VALUE|'${VUE_APP_NLU_ENTRIES_PAGE_SIZE}'|g' $file
+
+done
+echo "Starting Nginx"
+nginx -g 'daemon off;'
