6a3d789b0d2ddd09e3b5b4d89054104a561bb0a7
Omar MELLOULI <omar.mellouli12@gmail.com>
2022-04-16 23:43:27 +0200

    fixes #153 add public path env variable for admin build (#165)

--- admin/entrypoint.sh
+++ admin/entrypoint.sh
@@ -12,6 +12,7 @@ do
   sed -i 's|VUE_APP_NLU_SERVICE_URL_VALUE|'${VUE_APP_NLU_SERVICE_URL}'|g' $file 
   sed -i 's|VUE_APP_NLU_PATH_VALUE|'${VUE_APP_NLU_PATH}'|g' $file
   sed -i 's|VUE_APP_NLU_ENTRIES_PAGE_SIZE_VALUE|'${VUE_APP_NLU_ENTRIES_PAGE_SIZE}'|g' $file
+  sed -i 's|PUBLIC_PATH_VALUE|'${PUBLIC_PATH}'|g' $file
 
 done
 echo "Starting Nginx"
